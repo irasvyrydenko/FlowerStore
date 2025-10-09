f0b99343a555c478170c5c0fae84a647b7e061f4
Iryna Svyrydenko <svyrydenko.pn@edu.ucu.ua>
2025-10-09 08:56:17 +0300

    fLOWERcOLOR: deleted whItespacE

--- src/main/java/flower/store/FlowerColor.java
+++ src/main/java/flower/store/FlowerColor.java
@@ -1,7 +1,8 @@
 package flower.store;
 import lombok.Getter;
 @Getter
-public enum FlowerColor {RED("#FF0000"),
+public enum FlowerColor {
+    RED("#FF0000"),
     BLUE("#0000FF"),
     YELLOW("#FFDE21"), 
     WHITE("#FFFFFF");
