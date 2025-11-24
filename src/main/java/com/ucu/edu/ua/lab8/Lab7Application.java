cb184092b79541e14ef78c19abb21f1130ac5213
Iryna Svyrydenko <svyrydenko.pn@edu.ucu.ua>
2025-11-24 16:19:50 +0200

    added class Lab8Application

---
+++ src/main/java/com/ucu/edu/ua/lab8/Lab7Application.java
@@ -0,0 +1,31 @@
+package com.ucu.edu.ua.lab8;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Lab7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7Application.class, args);
+	}
+	@GetMapping("/list")
+	// @RequestMapping(method= RequestMethod.GET, path = "/list")
+	public List<String> getRandomUUDIs() {
+		UUID.randomUUID();
+		return Stream.generate(() -> UUID.randomUUID())
+		.map(UUID::toString)
+		.limit(10)
+		.toList();
+	}
+}
+
